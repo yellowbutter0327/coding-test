a0ea5162a83a9e42af15fd2dbec8a69b329f0966
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-06-08 23:15:02 +0900

    [PGS]level0/가까운 수,등차수열의 특정한 항만 더하기,특이한 이차원 배열2

---
+++ programmers/level0/가까운 수/가까운 수.js
@@ -0,0 +1,9 @@
+function solution(array, n) {
+  return array.reduce((a, c) =>
+    Math.abs(a - n) < Math.abs(c - n)
+      ? a
+      : Math.abs(a - n) === Math.abs(c - n)
+      ? Math.min(a, c)
+      : c
+  );
+}
