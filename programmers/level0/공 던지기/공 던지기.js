675b5ae83564e8d6e142f34866921a2a8086f248
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-06-17 23:11:18 +0900

    [PGS]level0/수열과 구간 쿼리1

--- programmers/level0/공 던지기/공 던지기.js
+++ programmers/level0/공 던지기/공 던지기.js
@@ -1,10 +1,3 @@
 function solution(numbers, k) {
-  let getBall = 1;
-  for (let i = 0; i < k - 1; i++) {
-    getBall += 2;
-    if (getBall > numbers.length - 1) {
-      getBall -= numbers.length;
-    }
-  }
-  return getBall;
+  return numbers[((k - 1) * 2) % numbers.length];
 }
