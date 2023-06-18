101e853f48d9ba520f55bbdb3c58f19ecffa716b
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-06-18 23:35:22 +0900

    [PGS]level0/빈 배열에 추가, 삭제하기

---
+++ programmers/level0/빈 배열에 추가, 삭제하기/빈 배열에 추가, 삭제하기.js
@@ -0,0 +1,15 @@
+function solution(arr, flag) {
+  var answer = [];
+  for (let i = 0; i < flag.length; i++) {
+    if (flag[i]) {
+      for (let j = 0; j < arr[i] * 2; j++) {
+        answer.push(arr[i]);
+      }
+    } else {
+      for (let k = 0; k < arr[i]; k++) {
+        answer.pop();
+      }
+    }
+  }
+  return answer;
+}
