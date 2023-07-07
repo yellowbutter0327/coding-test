9b921f569cd0a3621e27875d5ceb42e3e7dedb24
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-07-07 22:37:49 +0900

    [PGS]level0/소인수분해

---
+++ programmers/level0/소인수분해/소인수분해.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let result = [];
+  let divisor = 2;
+  while (n >= 2) {
+    if (n % divisor === 0) {
+      result.push(divisor);
+      n = n / divisor;
+    } else divisor++;
+  }
+  return [...new Set(result)];
+}
