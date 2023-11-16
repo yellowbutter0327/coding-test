89f90228aa9bc465ee0116be724f3c80a9b23442
joeunhye <slrkwhdmsp@naver.com>
2023-11-16 11:11:56 +0900

    [PGS](2번째)/level1/x만큼 간격이 있는 n개의 숫자

--- programmers/level1/ x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
+++ programmers/level1/ x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.js
@@ -1,7 +1,14 @@
 function solution(x, n) {
   let arr = [];
-  for (let i = 1; i < n + 1; i++) {
-    arr.push(x * i);
+  for (let i = 1; i <= n; i++) {
+    arr.push(i * x);
   }
   return arr;
+}
+
+//알게 된 것: Array.fill.map
+function solution(x, n) {
+  return Array(n)
+    .fill(x)
+    .map((v, i) => (i + 1) * v);
 }
