c5c3ef449a4c18b1d4ede07f98f3d676cb3e4ece
yellowbutter0327 <slrkwhdmsp@naver.com>
2023-11-18 23:55:46 +0900

    [PGS]평균 구하기/reduce 함수 알게 된 것 추가

--- programmers/level1/평균 구하기/평균 구하기.js
+++ programmers/level1/평균 구하기/평균 구하기.js
@@ -15,6 +15,7 @@ function solution(arr) {
   return result / arr.length;
 }
 
+//reduce (a,b,c) c가 오느냐 안 오느냐에 따라 index가 0으로 시작하냐 1로 시작하느냐가 바뀐다.
 function average(array) {
   return array.reduce((a, b) => a + b) / array.length;
 }
